270635db4cf0f34d4e64612ac096d9ba2d29e9ae
Niels Ballis <niels.ballis@asml.com>
2021-12-09 15:26:01 +0100

    almost there

--- day9/part2.ps1
+++ day9/part2.ps1
@@ -4,6 +4,26 @@ param (
     [switch]$test = $false
 )
 
+
+function Write-Grid {
+    param (
+    [Parameter(Mandatory=$true)]
+    [array]$grid
+    )
+    for($ypos=0;$ypos -lt $grid.Count;$ypos++) {
+        for($xpos=0;$xpos -lt $grid[$ypos].Count;$xpos++) {
+            if ($null -eq ($grid[$ypos][$xpos])) {
+                Write-Host ". " -NoNewline
+            }
+            else {
+                write-host "$($grid[$ypos][$xpos]) " -NoNewline
+            }   
+        }
+        write-host "`n"
+    }
+    Write-Host "---"
+}
+
 if ($test) {
     $in = Get-Content .\testinput.txt
 }
@@ -25,9 +45,6 @@ foreach ($line in $in){
 #        $grid[$ypos][$xpos] = [int]$line.Substring($xpos-1,1)
         if (($line.Substring($xpos-1,1) -ne "9") -and ($null -ne $line.Substring($xpos-1,1))) {
             $grid[$ypos][$xpos] = 1
-            $grid2[$ypos][$xpos] = 1
-            $grid3[$ypos][$xpos] = 1
-            $grid4[$ypos][$xpos] = 1
         }
     }
     $ypos++
@@ -35,122 +52,93 @@ foreach ($line in $in){
 
 for($ypos=1;$ypos -lt ($grid.Count - 1);$ypos++) {
     for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
-        if (($grid[$ypos][$xpos]) -and ($grid[$ypos][$xpos-1])) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos][$xpos-1])) {
             $grid[$ypos][$xpos] += $grid[$ypos][$xpos-1]
+            $grid[$ypos][$xpos-1] = 0
         }
     }
 }
-for($ypos=1;$ypos -lt ($grid.Count - 1);$ypos++) {
-    for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
-        if (($grid[$ypos][$xpos]) -and ($grid[$ypos-1][$xpos])) {
+
+Write-Grid $grid
+
+for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
+    for($ypos=1;$ypos -lt ($grid.Count - 1);$ypos++) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos-1][$xpos])) {
             $grid[$ypos][$xpos] += $grid[$ypos-1][$xpos]
+            $grid[$ypos-1][$xpos] = 0
         }
     }
 }
 
+Write-Grid $grid
 
-for($ypos=0;$ypos -lt $grid.Count;$ypos++) {
-    for($xpos=0;$xpos -lt $grid[$ypos].Count;$xpos++) {
-        if ($null -eq ($grid[$ypos][$xpos])) {
-            Write-Host ". " -NoNewline
-        }
-        else {
-            write-host "$($grid[$ypos][$xpos]) " -NoNewline
-        }   
-    }
-    write-host "`n"
-}
-
-Write-Host "----"
-
-for($ypos= ($grid2.Count - 2);$ypos -gt 0;$ypos--) {
-    for($xpos=($grid2[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
-        if (($grid2[$ypos][$xpos]) -and ($grid2[$ypos][$xpos+1])) {
-            $grid2[$ypos][$xpos] += $grid2[$ypos][$xpos+1]
-        }
-    }
-}
-for($ypos= ($grid2.Count - 2);$ypos -gt 0;$ypos--) {
-    for($xpos=($grid2[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
-        if (($grid2[$ypos][$xpos]) -and ($grid2[$ypos+1][$xpos])) {
-            $grid2[$ypos][$xpos] += $grid2[$ypos+1][$xpos]
+for($ypos=($grid.Count - 2);$ypos -gt 0;$ypos--) {
+    for($xpos=($grid[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos][$xpos+1])) {
+            $grid[$ypos][$xpos] += $grid[$ypos][$xpos+1]
+            $grid[$ypos][$xpos+1] = 0
         }
     }
 }
 
-for($ypos=0;$ypos -lt $grid2.Count;$ypos++) {
-    for($xpos=0;$xpos -lt $grid2[$ypos].Count;$xpos++) {
-        if ($null -eq ($grid2[$ypos][$xpos])) {
-            Write-Host ". " -NoNewline
+Write-Grid $grid
+
+for($xpos=($grid[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
+    for($ypos=($grid.Count - 2);$ypos -gt 0;$ypos--) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos+1][$xpos])) {
+            $grid[$ypos][$xpos] += $grid[$ypos+1][$xpos]
+            $grid[$ypos+1][$xpos] = 0
         }
-        else {
-            write-host "$($grid2[$ypos][$xpos]) " -NoNewline
-        }   
     }
-    write-host "`n"
 }
 
-Write-Host "----"
+Write-Grid $grid
 
-for($ypos=1;$ypos -lt ($grid3.Count - 1);$ypos++) {
+for($ypos=($grid.Count - 2);$ypos -gt 0;$ypos--) {
     for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
-        for($xpos=1;$xpos -lt ($grid3[$ypos].Count - 1);$xpos++) {
-            if (($grid3[$ypos][$xpos]) -and ($grid3[$ypos-1][$xpos])) {
-                $grid3[$ypos][$xpos] += $grid3[$ypos-1][$xpos]
-            }
-        }
-    }
-}
-for($ypos=1;$ypos -lt ($grid3.Count - 1);$ypos++) {
-    for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
-        if (($grid3[$ypos][$xpos]) -and ($grid3[$ypos][$xpos-1])) {
-            $grid3[$ypos][$xpos] += $grid3[$ypos][$xpos-1]
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos][$xpos-1])) {
+            $grid[$ypos][$xpos] += $grid[$ypos][$xpos-1]
+            $grid[$ypos][$xpos-1] = 0
         }
     }
 }
 
-for($ypos=0;$ypos -lt $grid3.Count;$ypos++) {
-    for($xpos=0;$xpos -lt $grid[$ypos].Count;$xpos++) {
-        if ($null -eq ($grid3[$ypos][$xpos])) {
-            Write-Host ". " -NoNewline
-        }
-        else {
-            write-host "$($grid3[$ypos][$xpos]) " -NoNewline
-        }   
-    }
-    write-host "`n"
-}
+Write-Grid $grid
 
-Write-Host "----"
-
-for($ypos= ($grid4.Count - 2);$ypos -gt 0;$ypos--) {
-    for($xpos=($grid4[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
-        if (($grid4[$ypos][$xpos]) -and ($grid4[$ypos+1][$xpos])) {
-            $grid4[$ypos][$xpos] += $grid4[$ypos+1][$xpos]
-        }
-    }
-}
-for($ypos= ($grid4.Count - 2);$ypos -gt 0;$ypos--) {
-    for($xpos=($grid4[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
-        if (($grid4[$ypos][$xpos]) -and ($grid4[$ypos][$xpos+1])) {
-            $grid4[$ypos][$xpos] += $grid4[$ypos][$xpos+1]
+for($xpos=1;$xpos -lt ($grid[$ypos].Count - 1);$xpos++) {
+    for($ypos=($grid.Count - 2);$ypos -gt 0;$ypos--) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos+1][$xpos])) {
+            $grid[$ypos][$xpos] += $grid[$ypos+1][$xpos]
+            $grid[$ypos+1][$xpos] = 0
         }
     }
 }
 
-for($ypos=0;$ypos -lt $grid4.Count;$ypos++) {
-    for($xpos=0;$xpos -lt $grid4[$ypos].Count;$xpos++) {
-        if ($null -eq ($grid4[$ypos][$xpos])) {
-            Write-Host ". " -NoNewline
+Write-Grid $grid
+
+for($ypos=1;$ypos -lt ($grid.Count - 1);$ypos++) {
+    for($xpos=($grid[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos][$xpos+1])) {
+            $grid[$ypos][$xpos] += $grid[$ypos][$xpos+1]
+            $grid[$ypos][$xpos+1] = 0
         }
-        else {
-            write-host "$($grid4[$ypos][$xpos]) " -NoNewline
-        }   
     }
-    write-host "`n"
 }
 
-Write-Host "----"
+Write-Grid $grid
+
+for($xpos=($grid[$ypos].Count - 2);$xpos -gt 0 ;$xpos--) {
+    for($ypos=1;$ypos -lt ($grid.Count - 1);$ypos++) {
+        if (($null -ne $grid[$ypos][$xpos]) -and ($null -ne $grid[$ypos-1][$xpos])) {
+            $grid[$ypos][$xpos] += $grid[$ypos-1][$xpos]
+            $grid[$ypos-1][$xpos] = 0
+        }
+    }
+}
+
+Write-Grid $grid
+
+
 <#
 [System.Collections.ArrayList]$lowpoints=@()
 
