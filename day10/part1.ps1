43488d5326a32af465084c2057fbb68c80ace5d1
Niels Ballis <n.ballis@hotmail.com>
2021-12-10 13:54:37 +0100

    part 2

--- day10/part1.ps1
+++ day10/part1.ps1
@@ -19,15 +19,15 @@ $Score = @("3","57","1197","25137")
 
 $answer = 0
 foreach ($line in $in){
-    [System.Collections.ArrayList]$Open = @()
+    [array]$Open = @()
     for($x=0;$x -lt $line.Length;$x++) {
         $character = $line.Substring($x,1)
         if ($OpenChars.Contains($character)) {
-            $Open += @{"char"=$character;"position"=$x}
+            $Open += $character
         }
         elseif ($CloseChars.Contains($character)) {
             $index = [array]::IndexOf($CloseChars,$character)
-            if ($Open[-1].char -eq $OpenChars[$index]){
+            if ($Open[-1] -eq $OpenChars[$index]){
                 $Open = $Open[0..($Open.Count-2)]
             }
             else {
--- day10/part2.ps1
+++ day10/part2.ps1
@@ -10,3 +10,50 @@ if ($test) {
 else {
     $in = Get-Content .\input.txt
 }
+
+
+
+$OpenChars = @("(","[","{","<")
+$CloseChars = @(")","]","}",">")
+#$Score = @("1","2","3","4")
+
+[array]$answers = @()
+foreach ($line in $in){
+    $answer = 0
+    $corrupt = $false
+    [array]$Open = @()
+    for($x=0;$x -lt $line.Length;$x++) {
+        $character = $line.Substring($x,1)
+        if ($OpenChars.Contains($character)) {
+            $Open += $character
+        }
+        elseif ($CloseChars.Contains($character)) {
+            $index = [array]::IndexOf($CloseChars,$character)
+            if ($Open[-1] -eq $OpenChars[$index]){
+                if ($Open.Count -gt 1) {
+                    $Open = $Open[0..($Open.Count-2)]
+                }
+                else {
+                    $Open = @()
+                }
+            }
+            else {
+                $corrupt = $true
+                $x = $line.Length
+            }
+        }
+    }
+    if (!($corrupt)) {
+        for($x=($Open.Count - 1);$x -ge 0;$x--) {
+            $Score = [array]::IndexOf($OpenChars,$Open[$x]) + 1
+            $answer = $answer * 5
+            $answer += $Score
+        }
+        $answers += $answer
+    }
+}
+
+($answers | Sort-Object)[($answers.count -1) / 2]
+
+
+
