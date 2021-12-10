61e54472e1747093cdbc80d411fe41d36f8408f4
Niels Ballis <n.ballis@hotmail.com>
2021-12-10 13:15:57 +0100

    part 1 working

--- day10/part1.ps1
+++ day10/part1.ps1
@@ -10,3 +10,32 @@ if ($test) {
 else {
     $in = Get-Content .\input.txt
 }
+
+
+
+$OpenChars = @("(","[","{","<")
+$CloseChars = @(")","]","}",">")
+$Score = @("3","57","1197","25137")
+
+$answer = 0
+foreach ($line in $in){
+    [System.Collections.ArrayList]$Open = @()
+    for($x=0;$x -lt $line.Length;$x++) {
+        $character = $line.Substring($x,1)
+        if ($OpenChars.Contains($character)) {
+            $Open += @{"char"=$character;"position"=$x}
+        }
+        elseif ($CloseChars.Contains($character)) {
+            $index = [array]::IndexOf($CloseChars,$character)
+            if ($Open[-1].char -eq $OpenChars[$index]){
+                $Open = $Open[0..($Open.Count-2)]
+            }
+            else {
+                $answer += $Score[$index]
+                $x = $line.Length
+            }
+        }
+    }
+}
+
+$answer
