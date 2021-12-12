20f1b4e76662ee6b9679ab2703064c103b9c8047
Niels Ballis <n.ballis@hotmail.com>
2021-12-12 22:33:15 +0100

    working with test input

--- day12/part1.ps1
+++ day12/part1.ps1
@@ -10,3 +10,63 @@ if ($test) {
 else {
     $in = Get-Content .\input.txt
 }
+
+[System.Collections.ArrayList]$connections=@()
+
+$start = "#"
+$end = "+"
+
+foreach ($line in $in){
+    $line = $line -replace "start",$start -replace "end",$end -split "-"
+    $connections.Add($line) | Out-Null
+}
+#$connections
+
+[System.Collections.ArrayList]$steps = @{}
+#[hashtable]$steps = @{}
+$steps += @{"0"=$start}
+
+#[System.Collections.ArrayList]$step = @()
+#$step.add(@("#","A"))
+#$step.add(@("#","b"))
+#$steps += @{"1"=$step}
+#$step.add(@(($steps.0),"A"))
+#$step.add(@(($steps.0),"b"))
+#$steps += @{"1"=$step}
+
+#$steps
+
+
+
+$count = 1
+$routecount = 0
+#$maxcount = 10
+$newstep = $true
+while($newstep) {
+    [array]$step = @()
+    foreach ($route in $steps.($count-1)) {
+        foreach ($connection in $connections) {
+            [string]$last = $route.substring($route.length-1,1)
+            if(($connection.Contains($last)) -and ($last -ne $end)) {
+                [string]$newcave = $connection[([array]::IndexOf($connection,$last) - 1)]
+                if ((!($route.Contains($newcave))) -or ($newcave -cmatch '[A-Z]')) {
+                    $step += $route + $newcave
+                    if ($newcave -eq $end) {
+                        $routecount++
+                    }
+                }
+            }
+        }
+    }
+    if ($step.Count){
+        $steps += @{"$count"=$step}
+    }
+    else {
+        $newstep=$false
+    }
+    $count++
+}
+
+$steps
+$routecount
+#>
