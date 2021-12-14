e70502c45ce50f3eb8a468fa4772fe7ce6b12bf0
Niels Ballis <niels.ballis@asml.com>
2021-12-14 11:19:43 +0100

    day 14 part 1

--- day14/part1.ps1
+++ day14/part1.ps1
@@ -13,3 +13,29 @@ else {
     write-error "File $inputfile not found!"
     throw
 }
+
+[string]$polymer = $in[0]
+[hashtable]$insert = @{}
+
+
+foreach ($line in $in[2..($in.Count-1)]) {
+    $line = $line -split " -> "
+    $insert += @{$line[0]=$line[1]}
+}
+
+$steps = 10
+
+for($step=1;$step -le $steps;$step++) {
+    [string]$newpolymer = $polymer.Substring(0,1)
+    for($x=0;$x -lt ($polymer.Length - 1);$x++) {
+        $newpolymer += $insert.($polymer.Substring($x,2)) + $polymer.Substring($x+1,1)
+    }
+    $polymer = $newpolymer
+}
+
+#$polymer.Length
+
+$out = $polymer.ToCharArray() | Group-Object | Sort-Object -Property "Count"
+#$out
+
+$out[-1].Count - $out[0].Count
