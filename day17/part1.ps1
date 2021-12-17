d22db7e86da52d55064ac1faeac799a70bc81ee3
Niels Ballis <n.ballis@hotmail.com>
2021-12-17 23:11:36 +0100

    day 16 part 1 not working

--- day17/part1.ps1
+++ day17/part1.ps1
@@ -13,3 +13,49 @@ else {
     write-error "File $inputfile not found!"
     throw
 }
+
+
+$xrange = (($in.Substring($in.IndexOf("x=")+2,$in.IndexOf(",")-($in.IndexOf("x=")+2))) -split "\.\.")
+$yrange = ($in.Substring($in.IndexOf("y=")+2)) -split "\.\."
+
+#$yrange
+
+[array]$maxhights = @()
+$hit = $true
+$starty = 1
+$yvelocity = $starty
+$maxhight = 0
+$y = 0
+do {
+    $y += $yvelocity
+    #Write-Host $y
+    if ($y -gt [int]$yrange[1]){
+        if ($y -gt $maxhight) {
+            $maxhight = $y
+        }
+        #Write-Host "Max $maxhight Y $y"
+        $yvelocity--
+    }
+    elseif ($y -ge [int]$yrange[0]) {
+        #write-host "HIT"
+        $starty++
+        $maxhights += $maxhight
+        $maxhight = 0
+        $yvelocity = $starty
+        $y = 0
+    }
+    else {
+        #write-host "no hit $starty"
+        if ($starty -gt 1000) {
+            $hit =$false
+        }
+        $starty++
+    }
+    
+} while ($hit)
+
+$maxhights.Count
+($maxhights | Measure-Object -Maximum).Maximum
+
+#2080 too low
+#>
